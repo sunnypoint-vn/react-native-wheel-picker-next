5c86e5500053d192bd6389e413f810c702ac92c7
tamlvm <tamlvm@sunnypoints.net>
2020-08-19 10:44:59 +0700

    move PickerIOS from react-native to react-native-community

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 import React from 'react';
 
 import {
-	PickerIOS,
 	Platform,
 } from 'react-native';
 
+import { Picker as PickerIOS } from '@react-native-community/picker'
 import WheelCurvedPicker from './WheelCurvedPicker'
 
 module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker)
